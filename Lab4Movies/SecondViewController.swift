58576e06416c4f2c9994c7963be650a1500024f2
Aaron Butler <aaronbutler@wustl.edu>
2019-10-03 16:03:32 -0500

    Initial Commit

---
+++ Lab4Movies/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Lab4Movies
+//
+//  Created by Aaron Butler on 10/3/19.
+//  Copyright © 2019 Aaron Butler. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
